099aca395debcbff5f3082d49f330f50f7124412
Nusvaa <yusufusman11111111@gmail.com>
2026-02-01 13:33:01 +0700

    More Progress

--- kualitasUdara.py
+++ kualitasUdara.py
@@ -3,20 +3,23 @@ import numpy as np
 import tkinter as tk
 from tkinter import ttk, filedialog, scrolledtext, messagebox
 import matplotlib.pyplot as plt
-import seaborn as sns
 
 class Backend:
     def __init__(self):
         self.df = None
+        self.df_clean = None
+        self.X_train, self.X_test = None, None
+        self.y_train, self.y_test = None, None
+        self.tes_df_mentah = None
+        self.X_min, self.X_max = None, None
         self.fitur = ['PM2_5','PM10','SO2','CO','NO2','Suhu','Kelembaban']
         self.label_map = {'Baik': 0, 'Sedang': 1, 'Tidak Sehat': 2, 'Berbahaya': 3}
 
-    def load_csv(self):
-        file_csv = filedialog.askopenfilenames()
+    def load_csv(self, file_csv):
         self.df = pd.read_csv(file_csv)
-        return "\n" + "=" * 20 + f"\nData berhasil dimuat.\nJumlah data = {len(self.df)} baris\n" + "=" * 20 + "\n"
+        return "\n" + "=" * 30 + f"\n>>> Data berhasil dimuat.\n>>> Jumlah data = {len(self.df)} baris\n" + "=" * 30 + "\n"
 
-    def preprocessing(self):
+    def preparation(self):
         missing_val = self.df.isna().sum().sum()
         duplicated_val = self.df.duplicated().sum().sum()
         data = self.df.dropna()
@@ -29,9 +32,9 @@ class Backend:
             data = data[(data[col] >= Q1 - 1.5 * IQR) & (data[col] <= Q3 + 1.5 * IQR)]
 
         self.df_clean = data
-        return "\n" + "=" * 20 + f"\nPreprocessing data selesai.\nJumlah data hilang = {missing_val}\nJumlah data duplikat = {duplicated_val}\nJumlah data setelah dibersihkan = {len.df_clean} baris\n" + "=" * 20 + "\n"
+        return "\n" + "=" * 30 + f"\n>>> Data Preparation selesai.\n>>> Jumlah data hilang = {missing_val}\n>>> Jumlah data duplikat = {duplicated_val}\n>>> Jumlah data setelah dibersihkan = {len(self.df_clean)} baris\n" + "=" * 30 + "\n"
 
-    def preparation(self):
+    def preprocessing(self):
         df_latih = self.df_clean.sample(frac=0.8, random_state=42)
         self.tes_df_mentah = self.df_clean.drop(df_latih.index)
 
@@ -43,7 +46,7 @@ class Backend:
         self.y_train = df_latih['Kategori'].map(self.label_map).values
         self.y_test = self.tes_df_mentah['Kategori'].map(self.label_map).values
 
-        return "\n" + "=" * 20 + '\nData Preprocessing Selesai: Split 80/20.\nNormalisasi Min-Max.\n' + '=' * 20 + '\n'
+        return "\n" + "=" * 30 + '\n>>> Data Preprocessing Selesai: Split 80/20.\n>>> Normalisasi Min-Max.\n' + '=' * 30 + '\n'
 
     def training(self):
         pass
@@ -55,7 +58,7 @@ class AppGUI:
     def __init__(self, root):
         self.root = root
         self.root.title("Program ML | Linear Regression Classification")
-        self.root.geometry("1200x750")
+        self.root.geometry("700x425")
 
         self.backend = Backend()
         self.widgets()
@@ -67,11 +70,11 @@ class AppGUI:
         ttk.Label(side, text="Memproses Data.").pack(pady=(0,5))
         self.btn_load = ttk.Button(side, text="Load Dataset", command=self.appLoad)
         self.btn_load.pack(fill=tk.X, pady=2)
-        self.btn_prep = ttk.Button(side, text="Preparation", command=self.appPrep)
+        self.btn_prep = ttk.Button(side, text="Preparation", state="disabled", command=self.appPrep)
         self.btn_prep.pack(fill=tk.X, pady=2)
-        self.btn_proc = ttk.Button(side, text="Processing", command=self.appProc)
+        self.btn_proc = ttk.Button(side, text="Processing", state="disabled", command=self.appProc)
         self.btn_proc.pack(fill=tk.X, pady=2)
-        self.btn_train = ttk.Button(side, text="Train", command=self.appTrain)
+        self.btn_train = ttk.Button(side, text="Train", state="disabled", command=self.appTrain)
         self.btn_train.pack(fill=tk.X, pady=2)
 
         ttk.Separator(side, orient="horizontal").pack(fill=tk.X, pady=15)
@@ -86,43 +89,64 @@ class AppGUI:
             ent.pack(side=tk.RIGHT, pady=1)
             self.entry = ent
 
-        self.btn_pred = ttk.Button(side, text="Prediksi Sekarang", command=self.appPred)
+        self.btn_pred = ttk.Button(side, text="Prediksi Sekarang", state="disabled", command=self.appPred)
         self.btn_pred.pack(fill=tk.X, pady=(10, 2))
 
-        self.log = scrolledtext.ScrolledText(side, width=35, height=10)
+        main = ttk.Frame(self.root, padding=10)
+        main.pack(side=tk.RIGHT, fill=tk.BOTH, expand=True)
+
+        self.log = scrolledtext.ScrolledText(main, width=35, height=10)
         self.log.pack(fill=tk.BOTH, expand=True, pady=10)
 
-        self.main = ttk.Frame(self.root, padding=10)
-        self.main.pack(side=tk.RIGHT, fill=tk.BOTH, expand=True)
-        self.plot_area = ttk.Frame(self.main)
-        self.plot_area.pack(fill=tk.BOTH, expand=True)
+        self.btn_reset = ttk.Button(main, text="Reset Log", command=self.reset)
+        self.btn_reset.pack(fill=tk.BOTH, pady=10)
 
-    def log(self, message):
-        self.log.insert(tk.END, f"> {message}\n")
+    def log_text(self, message):
+        self.log.insert(tk.END, f"{message}\n")
         self.log.see(tk.END)
 
     def reset(self):
         self.log.delete('1.0', tk.END)
 
     def appLoad(self):
-        pass
+        path = filedialog.askopenfilename(filetypes=[("CSV Files", "*.csv")])
+        if path:
+            self.log_text(self.backend.load_csv(path))
+            self.btn_prep.config(state='normal')
+            self.visual(self.backend.df, "Data Asli")
+        else:
+            messagebox.showerror("Gagal", "Gagal memuat data.")
     
     def appPrep(self):
-        pass
+        self.log_text(self.backend.preparation())
+        self.btn_proc.config(state='normal')
 
     def appProc(self):
-        pass
+        self.log_text(self.backend.preprocessing())
+        self.btn_train.config(state='normal')
 
     def appTrain(self):
-        pass
+        self.log_text(self.backend.training())
+        messagebox.showinfo("Sukses", "Model siap di gunakan!")
+        self.btn_pred.config(state='normal')
 
     def appPred(self):
         pass
 
     def visual(self, df, title):
-        pass
+        fig, ax = plt.subplots(figsize=(7,4))
+
+        for col in self.backend.fitur:
+            ax.scatter(range(len(df)), df[col], s=10, alpha=0.5)
+
+        ax.set_title(title)
+        ax.set_xlabel("Data Index")
+        ax.set_ylabel("Value")
+
+        fig.tight_layout()
+        plt.show()
 
 if __name__ == "__main__":
-    root = tk.Tk
+    root = tk.Tk()
     app = AppGUI(root)
     root.mainloop()
